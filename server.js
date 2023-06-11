60b1dd891a94a95a24e4ce5faed4fbcb55f97ae5
Enc <102102814+NicolasNazar@users.noreply.github.com>
2023-06-11 19:20:25 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const XLSX = require('xlsx');
 const fs = require('fs');
 const { format, parse } = require('date-fns');
 const { es } = require('date-fns/locale');
-
+const static = require('koa-static');
 const app = new Koa();
 const router = new Router();
 
@@ -42,10 +42,10 @@ router.get('/get_aforo', async (ctx, next) => {
   ctx.body = readDataFromExcelFiles();
 });
 
-router.get('/besthours', async (ctx, next) => {
+router.get('/besthours/:day', async (ctx, next) => {
   const data = readDataFromExcelFiles();
   const daysOfWeek = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes'];
-  const aforoThreshold = 100;
+  const aforoThreshold = 60;
   const hoursByDay = {};
 
   daysOfWeek.forEach(day => {
@@ -56,28 +56,40 @@ router.get('/besthours', async (ctx, next) => {
     if (aforo < aforoThreshold) {
       const date = parse(fecha, 'dd-MM-yyyy', new Date());
       const dayOfWeek = format(date, 'EEEE', { locale: es }).toLowerCase();
-      console.log(dayOfWeek)
+
       if (daysOfWeek.includes(dayOfWeek)) {
-        if (!hoursByDay[dayOfWeek][hora]) {
-          hoursByDay[dayOfWeek][hora] = 0;
+        // Redondear la hora al tramo de 2 horas más cercano
+        const [hour, minute, second] = hora.split(':').map(Number);
+        const roundedHour = Math.floor(hour )  ;
+        const roundedHora = `${roundedHour.toString().padStart(2, '0')}:00:00`;
+
+        if (!hoursByDay[dayOfWeek][roundedHora]) {
+          hoursByDay[dayOfWeek][roundedHora] = 0;
         }
-        hoursByDay[dayOfWeek][hora]++;
+        hoursByDay[dayOfWeek][roundedHora]++;
       }
     }
   });
 
   const bestHours = {};
-  daysOfWeek.forEach(day => {
-    const hours = hoursByDay[day];
-    const sortedHours = Object.entries(hours).sort((a, b) => b[1] - a[1]);
-    bestHours[day] = sortedHours.map(hour => hour[0]);
-  });
+  const day = ctx.params.day;
+  const hours = hoursByDay[day];
+  const sortedHours = Object.entries(hours)
+    .sort(([aHour], [bHour]) => {
+      const [aHourOnly] = aHour.split(':');
+      const [bHourOnly] = bHour.split(':');
+      return aHourOnly - bHourOnly;
+    })
+    .map(([hour]) => hour);
 
+  bestHours[day] = sortedHours;
   ctx.body = bestHours;
 });
+
 
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000, () => {
   console.log('Servidor Koa.js iniciado en el puerto 3000');
 });
+app.use(static(__dirname + '/')); 
