bbb842b375b75c4a4e0ac740733d04ee80710ea0
OneNico <69123659+OneNico@users.noreply.github.com>
2023-07-02 17:03:00 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,37 +5,15 @@ const fs = require('fs');
 const { format, parse } = require('date-fns');
 const { es } = require('date-fns/locale');
 const static = require('koa-static');
+const bodyParser = require('koa-bodyparser'); // Asegúrate de instalar este módulo
+
 const app = new Koa();
 const router = new Router();
 
+app.use(bodyParser());
+
 const readDataFromExcelFiles = () => {
-  const data = [];
-
-  fs.readdirSync(__dirname).forEach((file) => {
-    const matches = file.match(/^aforo_(\d{2})_(\d{2})/);
-
-    if (matches) {
-      const mes = parseInt(matches[1]);
-      const dia = parseInt(matches[2]);
-
-      const workbook = XLSX.readFile(file);
-      const sheet = workbook.Sheets[workbook.SheetNames[0]];
-
-      for (let rowNum = 2; sheet[`A${rowNum}`] && sheet[`B${rowNum}`]; rowNum++) {
-        const aforo = parseInt(sheet[`A${rowNum}`].v);
-        const fechaHoraNum = sheet[`B${rowNum}`].v;
-
-        const fechaHoraStr = XLSX.SSF.format('dd-mm-yyyy hh:mm:ss', fechaHoraNum);
-        const fechaHoraArr = fechaHoraStr.split(' ');
-        const fechaFormateada = fechaHoraArr[0];
-        const horaFormateada = fechaHoraArr[1];
-
-        data.push({ aforo, fecha: fechaFormateada, hora: horaFormateada });
-      }
-    }
-  });
-
-  return data;
+  // Tu código existente
 };
 
 router.get('/get_aforo', async (ctx, next) => {
@@ -43,53 +21,24 @@ router.get('/get_aforo', async (ctx, next) => {
 });
 
 router.get('/besthours/:day', async (ctx, next) => {
-  const data = readDataFromExcelFiles();
-  const daysOfWeek = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes'];
-  const aforoThreshold = 60;
-  const hoursByDay = {};
-
-  daysOfWeek.forEach(day => {
-    hoursByDay[day] = {};
-  });
-
-  data.forEach(({ aforo, fecha, hora }) => {
-    if (aforo < aforoThreshold) {
-      const date = parse(fecha, 'dd-MM-yyyy', new Date());
-      const dayOfWeek = format(date, 'EEEE', { locale: es }).toLowerCase();
-
-      if (daysOfWeek.includes(dayOfWeek)) {
-        // Redondear la hora al tramo de 2 horas más cercano
-        const [hour, minute, second] = hora.split(':').map(Number);
-        const roundedHour = Math.floor(hour )  ;
-        const roundedHora = `${roundedHour.toString().padStart(2, '0')}:00:00`;
-
-        if (!hoursByDay[dayOfWeek][roundedHora]) {
-          hoursByDay[dayOfWeek][roundedHora] = 0;
-        }
-        hoursByDay[dayOfWeek][roundedHora]++;
-      }
-    }
-  });
-
-  const bestHours = {};
-  const day = ctx.params.day;
-  const hours = hoursByDay[day];
-  const sortedHours = Object.entries(hours)
-    .sort(([aHour], [bHour]) => {
-      const [aHourOnly] = aHour.split(':');
-      const [bHourOnly] = bHour.split(':');
-      return aHourOnly - bHourOnly;
-    })
-    .map(([hour]) => hour);
-
-  bestHours[day] = sortedHours;
-  ctx.body = bestHours;
+  // Tu código existente
 });
 
+router.post('/submit_comment', async (ctx, next) => {
+    const comment = ctx.request.body.comment;
+    
+    if (comment && comment.trim()) {
+        fs.appendFileSync('comentarios.txt', `${comment}\n`);
+        ctx.status = 200;
+    } else {
+        ctx.status = 400;
+    }
+});
 
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000, () => {
   console.log('Servidor Koa.js iniciado en el puerto 3000');
 });
-app.use(static(__dirname + '/')); 
+
+app.use(static(__dirname + '/'));
